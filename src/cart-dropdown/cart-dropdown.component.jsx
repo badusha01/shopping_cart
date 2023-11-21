b4cb9fec9123c989539eae4146f8fdc6a7f1e6be
Badusha <badusha1818@gmail.com>
2023-11-21 12:43:03 +0530

    initial commit

---
+++ src/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,14 @@
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => {
+   return (
+    <div className='cart-dropdown-container'>
+        <div className='cart-items' />
+        <button>GO TO THE CHECKOUT</button>
+
+    </div>
+   )
+
+}
+
+export default CartDropdown;
