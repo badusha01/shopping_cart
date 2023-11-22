79f8609a5c11b1194bf1cbf503e95b1782b96215
Badusha <badusha1818@gmail.com>
2023-11-22 10:29:27 +0530

    created checkout page along with checkout items and checkout cart functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -32,24 +32,37 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
         )
 }
 
+     const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+     
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItem: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
-    cartCount: 0
+    clearItemFromCart: () => {},
+    cartCount: 0,
+    cartTotal: 0
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]); 
     const [cartCount, setCartCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
 
     useEffect(() => {
       const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
       setCartCount(newCartCount);
-    }, [cartItems])
+    }, [cartItems]);
+
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 
+        0
+        );
+        setCartTotal(newCartTotal);
+      }, [cartItems]);
 
     const addItemToCart = (productToAdd) => {
       setCartItems(addCartItem(cartItems, productToAdd))
@@ -58,14 +71,19 @@ export const CartProvider = ({ children }) => {
     const removeItemToCart = (cartItemToRemove) => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove))
       }
-  
+     const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear))
+      } 
 
     const value = { isCartOpen, 
         setIsCartOpen, 
         addItemToCart, 
         removeItemToCart, 
+        clearItemFromCart,
         cartItems, 
-        cartCount };
+        cartCount,
+        cartTotal, 
+    };
 
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
